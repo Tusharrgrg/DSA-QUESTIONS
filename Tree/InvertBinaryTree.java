fc28eb5fa5d6562fd5e2216162ea416355e2842c
Tushar agrawal <gargtushar495@gmail.com>
2023-02-18 14:46:11 +0530

    add invert tree Sol

---
+++ Tree/InvertBinaryTree.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class TreeNode {
+         TreeNode left;
+         TreeNode right;
+         int val;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+
+public class InvertBinaryTree{
+   
+    //recursive solution 
+    public TreeNode invertTree(TreeNode root){
+        if(root == null){
+            return null;
+        }
+
+        TreeNode temp = root.right;
+        root.right = invertTree(root.left);
+        root.left = invertTree(temp);
+
+        return root;
+
+    }
+
+    //iterative solution
+
+    public static TreeNode invertTree1(TreeNode root){
+        if(root == null){
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+
+            if(root.left!= null){
+                queue.offer(root.left);
+            }
+            if(root.right!= null){
+                queue.offer(root.right);
+            }
+        }
+        return root;
+    }
+}
