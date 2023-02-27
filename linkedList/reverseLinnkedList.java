f345865635c648c2191df93e18236389bb341162
Tushar agrawal <gargtushar495@gmail.com>
2023-02-27 20:21:06 +0530

    add solution

---
+++ linkedList/reverseLinnkedList.java
@@ -0,0 +1,22 @@
+class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+public class reverseLinnkedList{
+    public ListNode reverseList(ListNode head) {
+        
+        ListNode curr=head, prev=null;
+        
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+}
