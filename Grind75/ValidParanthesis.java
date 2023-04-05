1cceabfeead4b5eead3552d9dce0a390f7def5a7
Tushar agrawal <gargtushar495@gmail.com>
2023-04-06 01:54:31 +0530

    valid parenthesis

---
+++ Grind75/ValidParanthesis.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class ValidParanthesis {
+    
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) == '(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                st.push(s.charAt(i));
+            }else if(s.charAt(i) == ')' && !st.isEmpty()&& '(' == st.peek()){
+                st.pop();
+            }else if(s.charAt(i) == '}' && !st.isEmpty()&& '{' == st.peek()){
+                st.pop();
+            }else if(s.charAt(i) == ']' && !st.isEmpty()&& '[' == st.peek()){
+                st.pop();
+            }else{
+                return false;
+            }
+        }
+
+        if(!st.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+    
+}
