61156083eb6b86b3aecfc23dc173a5835d776330
Tushar agrawal <gargtushar495@gmail.com>
2023-04-18 19:10:35 +0530

    add soluiton

---
+++ CSES/DistinctNumbers.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class DistinctNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while(t-->0){
+            int n  = sc.nextInt();
+            HashMap<Long , Integer> map = new HashMap<>();
+
+            for(int i = 0; i<n ;i++){
+                long num = sc.nextLong();
+                map.put(num, map.getOrDefault(num, 0)+1);
+            }
+            System.out.println(map.size());
+        }
+    }
+}
